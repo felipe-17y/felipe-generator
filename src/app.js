967966faa42f081f3e0ac50a60667b621937bde7
felipe-17y <edwardfel@hotmail.com>
2023-10-05 01:09:24 +0000

    ...

--- src/app.js
+++ src/app.js
@@ -7,7 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let action = ["ate", "peed", "crushed", "broke"];
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
 
+fuction randomElement(Array){
+return array[Math.floor(math.random()* array.length)];
+}
+let excuse = 
+randomElement(who) +
+ " " +
+ randomElement(action) +
+ " " +
+ randomElement(what) +
+ " " +
+ randomElement(when); 
   console.log("Hello Rigo from the console!");
+  console.log(excuse);
 };
