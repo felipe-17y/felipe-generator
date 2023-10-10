b9fd9146de5ecee6647f14c68b169338376ae7e0
felipe-17y <edwardfel@hotmail.com>
2023-10-10 15:58:21 +0000

    generator

--- src/app.js
+++ src/app.js
@@ -1,28 +1,77 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'the keys', 'the car'];
-let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = crearExcusa();
+  });
+  console.log("!Albert is replacing Rigo!");
+};
 
-fuction randomElement(Array){
-return array[Math.floor(math.random()* array.length)];
-}
-let excuse = 
-randomElement(who) +
- " " +
- randomElement(action) +
- " " +
- randomElement(what) +
- " " +
- randomElement(when); 
-  console.log("Hello Rigo from the console!");
-  console.log(excuse);
+let crearExcusa = () => {
+  let pronombre = ["Un", "El", "Ese", "Este", "Aquel", "Otro"];
+  let sujeto = [
+    " comediante",
+    " taxista",
+    " patinador",
+    " conserje",
+    " feriante",
+    " perro"
+  ];
+  let accion = [
+    " se comió mis",
+    " rompió mis",
+    " perdió mis",
+    " robó mis",
+    " se fue con mis",
+    " regalo mis"
+  ];
+  let objeto = [
+    " deberes",
+    " trabajos",
+    " apuntes",
+    " boletos",
+    " zapatos",
+    " pantalones"
+  ];
+  let sitio = [
+    " en la calle",
+    " en el teatro",
+    " dónde nos conocimos",
+    " debajo de la cama",
+    " en mi casa"
+  ];
+  let cuando = [
+    " durante el camino",
+    " ayer",
+    " esta tarde",
+    " esta mañana",
+    " ayer por la noche",
+    " mientras estaba despistado",
+    " mientras dormia",
+    " al cruzar la calle"
+  ];
+
+  let pronombreIndex = Math.floor(Math.random() * pronombre.length);
+  let sujetoIndex = Math.floor(Math.random() * sujeto.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let objetoIndex = Math.floor(Math.random() * objeto.length);
+  let sitioIndex = Math.floor(Math.random() * sitio.length);
+  let cuandoIndex = Math.floor(Math.random() * cuando.length);
+
+  return (
+    pronombre[pronombreIndex] +
+    "" +
+    sujeto[sujetoIndex] +
+    "" +
+    accion[accionIndex] +
+    "" +
+    objeto[objetoIndex] +
+    "" +
+    sitio[sitioIndex] +
+    "" +
+    cuando[cuandoIndex]
+  );
 };
